646354d1c9a48fed73ecaf1b4f6cab49fc3d4a06
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 15:46:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 numbers = [1, 2, 3]
 
 def square_array(numbers)
-  numbers = []
   counter = 0
 while counter < numbers.length do
-  puts numbers [|i| numbers << i ** 2]
+  puts numbers [counter ** 2]
   counter +=1
 end
 numbers
