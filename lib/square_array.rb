0e0148239100ac9b0cea6e31dd34c7a388ca49d6
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 15:54:43 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+# call on each element
+# square each element to update the array
+
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  count = 0
+while count < numbers.length do
+  puts numbers[count] = numbers[count] ** 2
+  count += 1
 end
+numbers
+end
+  
