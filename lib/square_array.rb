84f81cbd79fbd480a42b45307b1a0c8c56916f4b
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 15:41:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  counter = 0
+while numbers.length[counter ** 2] do
+  p numbers[counter ** 2]
+  counter +=1
+end
 end
