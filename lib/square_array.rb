51c724571ac5983fec666fa82e2f157a914f0de3
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 15:51:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,13 @@
+# call on each element
+# square each element to update the array
+
 numbers = [1, 2, 3]
 
 def square_array(numbers)
   counter = 0
 while counter < numbers.length do
-  puts (numbers ** 2)
+  puts numbers[counter] ** 2
+  #puts (numbers ** 2)
   counter +=1
 end
 end
